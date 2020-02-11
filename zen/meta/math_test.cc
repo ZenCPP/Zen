edfc50dc70b64f8f6ebdd6d3a26fe382470d4b4b
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-02-11 15:58:28 +0100

    Add new single-header template metaprogramming library

--- zen/meta/math_test.cc
+++
@@ -1,9 +0,0 @@
-
-#include "zen/meta/math.hpp"
-
-using namespace zen;
-
-static_assert(zen::fac_t<zen::u3_>::value == 6);
-static_assert(zen::eval_t<zen::if_<zen::bool_<1 == 1>, zen::u1_, zen::u2_>>::value == 1, "");
-
-
--- zen/meta/maybe.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef ZEN_META_MAYBE_HPP
-#define ZEN_META_MAYBE_HPP
-
-#include "zen/meta/maybe/types.hpp"
-#include "zen/meta/maybe/iterator.hpp"
-
-#endif // ZEN_META_MAYBE_HPP
