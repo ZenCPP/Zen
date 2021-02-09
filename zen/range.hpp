8b0ef29dc7f45b9375cc9acea069e36e69689406
Sam Vervaeck <samvv@pm.me>
2021-02-09 13:25:53 +0100

    Format zen::is_range_<T>

--- zen/range.hpp
+++ zen/range.hpp
@@ -9,13 +9,14 @@ namespace zen {
   struct is_range_ : std::false_type {};
 
   template <typename T>
-  struct is_range_<T,
+  struct is_range_<
+      T,
       std::void_t<
            decltype(std::declval<T&>().begin()),
            decltype(std::declval<T&>().end()),
            typename T::value_type
-         >>
-      : std::true_type {};
+         >
+     > : std::true_type {};
 
   template<typename T>
   using is_range_v = typename is_range_<T>::value;
