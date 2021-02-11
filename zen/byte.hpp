b9cdbca0efa8cc3683981bcf46828293ea1c5867
Sam Vervaeck <samvv@pm.me>
2021-02-11 19:00:09 +0100

    Add zen::ByteString as an alias to std::basic_string

--- zen/byte.hpp
+++ zen/byte.hpp
@@ -1,11 +1,15 @@
 #ifndef ZEN_BYTE_HPP
 #define ZEN_BYTE_HPP
 
+#include <string>
+
 #include "zen/config.h"
 
 ZEN_NAMESPACE_START
 
 using Byte = unsigned char;
+
+using ByteString = std::basic_string<Byte>;
 
 #define ZEN_BYTE_LITERAL(literal) ((const Byte*)(literal))
 
