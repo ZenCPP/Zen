b98a1d20f04655e7124aa4a02ec373472195ccd8
Sam Vervaeck <samvv@pm.me>
2021-02-13 12:02:57 +0100

    Fix tests of meta.hpp

--- zen/meta_test.cc
+++ zen/meta_test.cc
@@ -1,11 +1,18 @@
 
+#if ZEN_STL
+#include <string>
+#endif
+
 #include "zen/meta.hpp"
 
 using namespace ZEN_NAMESPACE;
 
-static_assert(zen::size_v<list_t<u1_, u2_>> == 2);
+static_assert(Size<Tuple<U1, U2>>::value == 2);
 
-using p1 = rcons_t<u1_, list_t<u1_>>;
+using P1 = PrependT<U1, Tuple<U1>>;
 
-static_assert(size_v<p1> == 2);
+static_assert(Size<P1>::value == 2);
 
+#if ZEN_STL
+static_assert(IsContainer<std::string>::value == true);
+#endif
