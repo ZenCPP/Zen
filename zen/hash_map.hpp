cd3ca797c83d7bc1cb4a764ea592c66bb3c02532
Sam Vervaeck <samvv@pm.me>
2021-02-13 12:03:30 +0100

    Add a stub for HashMap<T>

---
+++ zen/hash_map.hpp
@@ -0,0 +1,23 @@
+#ifndef ZEN_HASH_MAP_HPP
+#define ZEN_HASH_MAP_HPP
+
+#include <stddef.h>
+
+#include "zen/allocator.hpp"
+
+ZEN_NAMESPACE_START
+
+template<
+  typename T,
+  typename SizeT = size_t,
+  typename AllocatorT = DefaultAllocator<T>
+>
+class HashMap {
+
+public:
+
+};
+
+ZEN_NAMESPACE_END
+
+#endif // of #ifndef ZEN_HASH_MAP_HPP
